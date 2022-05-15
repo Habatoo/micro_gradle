cd2034610ac83974f607999543a6eb6ea67368cc
habatoo <habatoo@gmail.com>
2022-05-15 17:17:26 +0700

    initial commit

---
+++ src/main/java/com/example/micro_gradle/MicroGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.micro_gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroGradleApplication.class, args);
+	}
+
+}
